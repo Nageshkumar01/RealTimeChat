4bc4f8425363f6c0a27d8c9ec552039a77f93438
Nageshkumar01 <160787005+Nageshkumar01@users.noreply.github.com>
2026-06-22 10:18:15 +0530

    Refactor index.js for improved configuration and routes

--- full-stack_chatApp-main/backend/src/index.js
+++ full-stack_chatApp-main/backend/src/index.js
@@ -1,26 +1,22 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
@@ -28,15 +24,11 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// Health check route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
- app.get("",(req,res)=>
-res.send("this is home directory"));
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
