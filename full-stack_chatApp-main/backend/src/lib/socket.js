5ebdc631c0ea16fe634e68cf81151833045d1fbd
Nageshkumar01 <160787005+Nageshkumar01@users.noreply.github.com>
2026-06-22 10:53:58 +0530

    Refactor socket.js for improved clarity and functionality

--- full-stack_chatApp-main/backend/src/lib/socket.js
+++ full-stack_chatApp-main/backend/src/lib/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST"],
   },
 });
 
@@ -15,21 +17,27 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+// Store online users
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+  console.log("A user connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    console.log("A user disconnected:", socket.id);
+
+    if (userId) {
+      delete userSocketMap[userId];
+    }
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
